9943a267c666fa847df561f65e23ce465e107df2
Julien Maitrehenry <julien.maitrehenry@me.com>
2020-04-15 17:03:27 -0400

    A lot of fixes

--- docker/redis/entrypoint_redis.sh
+++ docker/redis/entrypoint_redis.sh
@@ -3,46 +3,57 @@ REDIS_PORT=${REDIS_PORT:-6379}
 
 REDIS_MASTER=${REDIS_MASTER:-redis}
 
-REDIS_SENTINEL=${REDIS_SENTINEL:-sentinel}
-REDIS_SENTINEL_PORT=${REDIS_SENTINEL_PORT:-26379}
+REDIS_SENTINEL_SERVICE_HOST=${REDIS_SENTINEL_SERVICE_HOST:-sentinel}
+REDIS_SENTINEL_SERVICE_PORT=${REDIS_SENTINEL_SERVICE_PORT:-26379}
 
-MAX_ATTEMPT=${MAX_ATTEMPT:-30}
-if  [[ ! -f /data/redis.conf ]]; then
-  i=0
-  while true; do
-    # No configuration present, try to find a master from existing sentinel
-    master=$(redis-cli -h ${REDIS_SENTINEL} -p ${REDIS_SENTINEL_PORT} --csv SENTINEL get-master-addr-by-name mymaster | grep -v "NIL"  | tr ',' ' ' | cut -d' ' -f1)
-    if [[ -n "${master}" ]]; then
-      echo "A master is found using sentinel, try to connect to it..."
-      echo $master
-      master="${master//\"}"
-    else
-      echo "No master or sentinel found, try to connect to a backup redis host..."
-      role=$(redis-cli -h $REDIS_MASTER -p $REDIS_PORT INFO REPLICATIon | grep role | dos2unix)
-      if [[ "role:master" == "$role" ]]; then
-        echo "Redis master founded!"
-        master=$REDIS_MASTER
-      fi 
+MAX_ATTEMPT=${MAX_ATTEMPT:-10}
+
+cp /opt/redis/redis.conf.template /tmp/redis.conf
+i=0
+while true; do
+  # No configuration present, try to find a master from existing sentinel
+  master=$(timeout 1 redis-cli -h ${REDIS_SENTINEL_SERVICE_HOST} -p ${REDIS_SENTINEL_SERVICE_PORT} --csv SENTINEL get-master-addr-by-name mymaster | grep -v "NIL"  | tr ',' ' ' | cut -d' ' -f1)
+  if [[ -n "${master}" ]]; then
+    echo "A master is found using sentinel, try to connect to it..."
+    echo $master
+    master="${master//\"}"
+    redis cli -h $master -p $REDIS_PORT PING
+    if [[ $? -ne 0 ]]; then
+      echo "$master didn't respond to PING request..."
+      master=""
     fi
+  fi
 
-    if [[ -n "$master" ]]; then
-    # transform hostname to IP
-      master=$(getent hosts $master | cut -d' ' -f1)
+  if [[ -n "$master" ]]; then
+  # transform hostname to IP
+    master=$(getent hosts $master | cut -d' ' -f1)
 
-      echo "Generate redis config..."
-      echo "slaveof $master $REDIS_PORT"
-      echo "slaveof $master $REDIS_PORT" >> /data/redis.conf
-      break
-    fi
+    echo "Redis master founded!"
+    echo "slaveof $master $REDIS_PORT" >> /tmp/redis.conf
+    break
+  fi
+  
+  # Giveup to try with sentinel
+  if [[ $i -ge $MAX_ATTEMPT ]]; then
+    break;
+  fi
 
-    echo "No master found..."
-    if [[ $i -ge $MAX_ATTEMPT ]]; then
-      echo "Start as a new redis master"
-      break;
-    fi
-    i=$(($i+1))
-    sleep 1
-  done
-fi
+  i=$(($i+1))
+  sleep 1
+done
 
-redis-server /data/redis.conf --protected-mode no "$@"
+
+echo "No master found using sentinel, try to connect to a backup redis host..."
+role=$(timeout 1 redis-cli -h $REDIS_MASTER -p $REDIS_PORT INFO REPLICATIon | grep role | dos2unix)
+if [[ "role:master" == "$role" ]]; then
+  echo "Redis master founded!"
+  master=$REDIS_MASTER
+  master=$(getent hosts $master | cut -d' ' -f1)
+  echo "slaveof $master $REDIS_PORT" >> /tmp/redis.conf
+else
+  echo "No master found..."
+  echo "Start as a new redis master"
+fi 
+
+
+redis-server /tmp/redis.conf --protected-mode no "$@"
--- docker/sentinel/entrypoint_sentinel.sh
+++ docker/sentinel/entrypoint_sentinel.sh
@@ -9,13 +9,13 @@ REDIS_SENTINEL_PORT=${REDIS_SENTINEL_PORT:-26379}
 if  [[ ! -f /data/sentinel.conf ]]; then
   while true; do
     # No configuration present, try to find a master from existing sentinel
-    master=$(redis-cli -h ${REDIS_SENTINEL} -p ${REDIS_SENTINEL_PORT} --csv SENTINEL get-master-addr-by-name mymaster | tr ',' ' ' | cut -d' ' -f1)
+    master=$(timeout 1 redis-cli -h ${REDIS_SENTINEL} -p ${REDIS_SENTINEL_PORT} --csv SENTINEL get-master-addr-by-name mymaster | tr ',' ' ' | cut -d' ' -f1)
     if [[ -n "${master}" ]]; then
       echo "A master is found using a sentinel, try to connect to it..."
       master="${master//\"}"
     else
       echo "No master or sentinel found, try to connect to a backup redis host..."
-      role=$(redis-cli -h $REDIS_MASTER -p $REDIS_PORT INFO REPLICATIon | grep role | dos2unix)
+      role=$(timeout 1 redis-cli -h $REDIS_MASTER -p $REDIS_PORT INFO REPLICATIon | grep role | dos2unix)
       if [[ "role:master" == "$role" ]]; then
         echo "Redis master founded!"
         master=$REDIS_MASTER
@@ -29,8 +29,8 @@ if  [[ ! -f /data/sentinel.conf ]]; then
       echo "Generate sentinel config..."
       echo "dir /data" > /data/sentinel.conf
       echo "sentinel monitor mymaster $master $REDIS_PORT 2" >> /data/sentinel.conf
-      echo "sentinel down-after-milliseconds mymaster 60000" >> /data/sentinel.conf
-      echo "sentinel failover-timeout mymaster 180000" >> /data/sentinel.conf
+      echo "sentinel down-after-milliseconds mymaster 1000" >> /data/sentinel.conf
+      echo "sentinel failover-timeout mymaster 60000" >> /data/sentinel.conf
       echo "sentinel parallel-syncs mymaster 1" >> /data/sentinel.conf
       echo "bind 0.0.0.0" >> /data/sentinel.conf
       break
